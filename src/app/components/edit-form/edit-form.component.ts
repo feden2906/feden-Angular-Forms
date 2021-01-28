f25eb1023bc87210d5cf8387c6d26fd975f0858c
freefireman <beluh.c.s@gmail.com>
2021-01-28 21:22:08 +0200

    little fix))

--- src/app/components/edit-form/edit-form.component.ts
+++ src/app/components/edit-form/edit-form.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {User} from 'src/app/models/user';
 
 @Component({
   selector: 'app-edit-form',
@@ -9,78 +10,27 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class EditFormComponent implements OnInit {
 
   @Input()
-  editingUser = {
-    address: {
-      street: '',
-      suite: '',
-      city: '',
-      zipcode: '',
-      geo: {
-        lat: '',
-        lng: ''
-      }
-    },
-    company: {
-      name: '',
-      catchPhrase: '',
-      bs: ''
-    },
-    email: '',
-    id: 0,
-    name: '',
-    phone: '',
-    username: '',
-    website: '',
-  };
+  editingUser: User;
+
   @Output()
   bubbleUp = new EventEmitter();
 
-  name = new FormControl(this.editingUser.name);
-  username = new FormControl(this.editingUser.username);
-  email = new FormControl(this.editingUser.email);
-  street = new FormControl(this.editingUser.address.street);
-  suite = new FormControl(this.editingUser.address.suite);
-  city = new FormControl(this.editingUser.address.city);
-  zipcode = new FormControl(this.editingUser.address.zipcode);
-  lat = new FormControl(this.editingUser.address.geo.lat);
-  lng = new FormControl(this.editingUser.address.geo.lng);
-  phone = new FormControl(this.editingUser.phone);
-  website = new FormControl(this.editingUser.website);
-  companyName = new FormControl(this.editingUser.company.name);
-  catchPhrase = new FormControl(this.editingUser.company.catchPhrase);
-  bs = new FormControl(this.editingUser.company.bs);
+  myForm: FormGroup;
 
-  editUser = new FormGroup({
-    name: this.username,
-    username: this.username,
-    email: this.email,
-    address: {
-      street: this.street,
-      suite: this.suite,
-      city: this.city,
-      zipcode: this.zipcode,
-      geo: {
-        lat: this.lat,
-        lng: this.lng,
-      }
-    },
-    phone: this.phone,
-    website: this.website,
-    company: {
-      name: this.companyName,
-      catchPhrase: this.catchPhrase,
-      bs: this.bs,
-    }
-  });
-
-  constructor() {
-
-  }
+  constructor(private formBuilder: FormBuilder){}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
   closeForm(): void {
     this.bubbleUp.emit('close');
+  }
+
+  private initForm(): void {
+   this.myForm = this.formBuilder.group({
+     name: this.editingUser.name,
+     username: this.editingUser.username,
+   });
   }
 }
